f76f74363a16aa7c3c1a2c17067f9958d5cabcad
Nicolas Zacharis <nzacharis@gatech.edu>
2022-12-03 23:24:51 -0500

    Added top 2 screen time apps to notification

--- app/src/main/java/com/example/mobilelock/ReminderBroadcast.java
+++ app/src/main/java/com/example/mobilelock/ReminderBroadcast.java
@@ -1,23 +1,209 @@
 package com.example.mobilelock;
 
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.text.format.DateUtils;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
 public class ReminderBroadcast extends BroadcastReceiver {
+    private static final boolean localLOGV = false;
+    private UsageStatsManager mUsageStatsManager;
+    private LayoutInflater mInflater;
+    private ReminderBroadcast.UsageStatsAdapter mAdapter;
+    private PackageManager mPm;
+    private static final String TAG = "BroadcastActivity";
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        mUsageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mPm = context.getPackageManager();
+        mAdapter = new UsageStatsAdapter();
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Notifications")
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("Busy Day Tomorrow.")
-                .setContentText("Limit APPs tommorow to buy yourself time")
+                .setContentText("Limit " + mAdapter.getItem(0).getPackageName() + " and " + mAdapter.getItem(1).getPackageName() + " tommorow to buy yourself time")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         notificationManager.notify(200, builder.build());
     }
 
+
+    public static class AppNameComparator implements Comparator<UsageStats> {
+        private Map<String, String> mAppLabelList;
+
+        AppNameComparator(Map<String, String> appList) {
+            mAppLabelList = appList;
+        }
+
+        @Override
+        public final int compare(UsageStats a, UsageStats b) {
+            String alabel = mAppLabelList.get(a.getPackageName());
+            String blabel = mAppLabelList.get(b.getPackageName());
+            return alabel.compareTo(blabel);
+        }
+    }
+
+
+    public static class UsageTimeComparator implements Comparator<UsageStats> {
+        @Override
+        public final int compare(UsageStats a, UsageStats b) {
+            return (int)(b.getTotalTimeInForeground() - a.getTotalTimeInForeground());
+        }
+    }
+
+    // View Holder used when displaying views
+    static class AppViewHolder {
+        TextView pkgName;
+        TextView lastTimeUsed;
+        TextView usageTime;
+    }
+
+    class UsageStatsAdapter extends BaseAdapter {
+        // Constants defining order for display order
+        private static final int _DISPLAY_ORDER_USAGE_TIME = 0;
+        private static final int _DISPLAY_ORDER_APP_NAME = 1;
+
+        private int mDisplayOrder = _DISPLAY_ORDER_USAGE_TIME;
+        private ReminderBroadcast.UsageTimeComparator mUsageTimeComparator = new ReminderBroadcast.UsageTimeComparator();
+        private ReminderBroadcast.AppNameComparator mAppLabelComparator;
+        public final ArrayMap<String, String> mAppLabelMap = new ArrayMap<>();
+        public final ArrayList<UsageStats> mPackageStats = new ArrayList<>();
+
+        UsageStatsAdapter() {
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DAY_OF_YEAR, -5);
+
+            final List<UsageStats> stats =
+                    mUsageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST,
+                            cal.getTimeInMillis(), System.currentTimeMillis());
+            if (stats == null) {
+                return;
+            }
+
+            ArrayMap<String, UsageStats> map = new ArrayMap<>();
+            final int statCount = stats.size();
+            for (int i = 0; i < statCount; i++) {
+                final android.app.usage.UsageStats pkgStats = stats.get(i);
+
+                // load application labels for each application
+                try {
+                    ApplicationInfo appInfo = mPm.getApplicationInfo(pkgStats.getPackageName(), 0);
+                    String label = appInfo.loadLabel(mPm).toString();
+                    mAppLabelMap.put(pkgStats.getPackageName(), label);
+
+                    UsageStats existingStats =
+                            map.get(pkgStats.getPackageName());
+                    if (existingStats == null) {
+                        map.put(pkgStats.getPackageName(), pkgStats);
+                    } else {
+                        existingStats.add(pkgStats);
+                    }
+
+                } catch (PackageManager.NameNotFoundException e) {
+                    // This package may be gone.
+                }
+            }
+            mPackageStats.addAll(map.values());
+
+            // Sort list
+            mAppLabelComparator = new ReminderBroadcast.AppNameComparator(mAppLabelMap);
+            sortList();
+        }
+
+        @Override
+        public int getCount() {
+            return mPackageStats.size();
+        }
+
+        @Override
+        public UsageStats getItem(int position) {
+            return mPackageStats.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // A ViewHolder keeps references to children views to avoid unneccessary calls
+            // to findViewById() on each row.
+            ReminderBroadcast.AppViewHolder holder;
+
+            // When convertView is not null, we can reuse it directly, there is no need
+            // to reinflate it. We only inflate a new View when the convertView supplied
+            // by ListView is null.
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.usage_stats_item, null);
+
+                // Creates a ViewHolder and store references to the two children views
+                // we want to bind data to.
+                holder = new ReminderBroadcast.AppViewHolder();
+                holder.pkgName = (TextView) convertView.findViewById(R.id.package_name);
+                holder.usageTime = (TextView) convertView.findViewById(R.id.usage_time);
+                convertView.setTag(holder);
+            } else {
+                // Get the ViewHolder back to get fast access to the TextView
+                // and the ImageView.
+                holder = (ReminderBroadcast.AppViewHolder) convertView.getTag();
+            }
+
+            // Bind the data efficiently with the holder
+            UsageStats pkgStats = mPackageStats.get(position);
+            if (pkgStats != null) {
+                String label = mAppLabelMap.get(pkgStats.getPackageName());
+                holder.pkgName.setText(label);
+                holder.usageTime.setText(
+                        DateUtils.formatElapsedTime(pkgStats.getTotalTimeInForeground() / 1000));
+            } else {
+                Log.w(TAG, "No usage stats info for package:" + position);
+            }
+            return convertView;
+        }
+
+        void sortList(int sortOrder) {
+            if (mDisplayOrder == sortOrder) {
+                // do nothing
+                return;
+            }
+            mDisplayOrder= sortOrder;
+            sortList();
+        }
+        private void sortList() {
+            if (mDisplayOrder == _DISPLAY_ORDER_USAGE_TIME) {
+                if (localLOGV) Log.i(TAG, "Sorting by usage time");
+                Collections.sort(mPackageStats, mUsageTimeComparator);
+            } else if (mDisplayOrder == _DISPLAY_ORDER_APP_NAME) {
+                if (localLOGV) Log.i(TAG, "Sorting by application name");
+                Collections.sort(mPackageStats, mAppLabelComparator);
+            }
+            notifyDataSetChanged();
+        }
+    }
 }
